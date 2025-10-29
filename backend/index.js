77b0051118ce13f2e2124b56ac7cabd702290c3d
Kamal0921 <727823tuee045@skct.edu.in>
2025-10-29 14:06:27 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const courseRoutes = require('./routes/courseRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
+
+// CORS
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || '*'
+}));
 app.use(express.json());
 
+// MongoDB connection
+const DB_NAME = 'knowledge_db';
 
-mongoose.connect('mongodb+srv://Kamal:Kamal2006@cluster0.aoshqsw.mongodb.net/Course?retryWrites=true&w=majority&appName=Cluster0')
-.then(()=>console.log("Connected to MongoDB"))
-.catch((err)=>console.log(err));
+const YOUR_HARDCODED_URL = "mongodb+srv://Kamal:Kamal2006@cluster0.aoshqsw.mongodb.net/?appName=Cluster0";
 
-
-//routing
-app.post('/courses',() => {
-    res.send("welcome to portal");
+mongoose.connect(YOUR_HARDCODED_URL, {
+  dbName: DB_NAME, 
+  appName: 'Cluster0'
 })
-app.listen(3000,()=>{
-    console.log("port is listening");
-})
+  .then(() => console.log(`MongoDB connected to database: ${DB_NAME}`)) // This should now appear
+  .catch(err => console.log('MongoDB connection error:', err.message));
 
+// Routes
+app.use('/api/courses', courseRoutes);
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => res.send('Knowledge Delivery Platform API running'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
